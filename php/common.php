f9a110008d1294dd51bf35b4da9b87f3311e14b5
Cristina Tincu <33434867+cristinatincu@users.noreply.github.com>
2019-02-21 20:42:38 +0000

    Add files via upload

--- php/common.php
+++ php/common.php
@@ -37,6 +37,44 @@
 
     }
 
+    function userforms(){
+        print('
+            <aside class="grid-item">
+
+                <script src="../js/formsChange.js"></script>
+                <script src="../js/validation.js"></script>
+
+                <p id = "error"></p>
+                
+                <form id = "login" onsubmit="return isLogInValid()">
+                    <input type="text" placeholder="Email" id = "email" autofocus required/>           
+                    <input type="password" placeholder="Password" id = "passwd" required/>
+                    <button type="submit">Log in</button>
+                    <center>
+                        <p style="font-weight: bold; font-size: 1.2rem;">Do not have an account?</p>
+                    </center>          
+                    <button type="button" onclick="changeToRegister()">Register</button>              
+                </form>
+                    
+                <form id="register" onsubmit="return isRegisterValid()">
+                    <input type="text" placeholder="Username" id = "usernameReg" name = "userbnameReg" autofocus required/>
+                    <input type="password" placeholder="Password" id = "passwdReg" name = "passwdReg"required/>
+                    <input type="password" placeholder="Repeat Password" id = "passwdRepeat" name = "passwdRepeat"required/>
+                    <input type="email" placeholder="Email" id = "emailReg" name = "emailReg" required/>
+                    <button type="submit">Register</button>
+                    <center>
+                        <p style="font-weight: bold; font-size: 1.2rem;">Already have an account?</p>
+                    </center> 
+                    <button type="button" onclick="changeToLogin()">Log in</button>
+                </form>
+
+                <div id = "userInfo">
+                    <p id = "loggedInUser"></p>
+                    
+                </div>     
+            </aside>');
+    }
+
     //Generates the footer
     function footer(){
         print('
