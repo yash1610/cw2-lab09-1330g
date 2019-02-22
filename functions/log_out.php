7d19e8a12cb6905c0fcaf9eefc088f20cf06a408
Yash Bhardwaj <yphantom1610@gmail.com>
2019-02-22 07:38:55 +0000

    Add files via upload

---
+++ functions/log_out.php
@@ -0,0 +1,12 @@
+<?php
+    //Start session management
+    session_start();
+
+    //Remove all session variables
+    session_unset(); 
+
+    //Destroy the session 
+    session_destroy(); 
+
+    //Echo result to user
+    echo 'ok';
---
+++ functions/recommendations.php
@@ -0,0 +1,33 @@
+<?php
+//Connect to database
+$mongoClient = new MongoClient();
+
+//Select a database
+$db = $mongoClient->cw2;
+
+//Select a collection 
+$collection = $db->user;
+
+//Extract the data that was sent to the server, bobby tables
+$search_string = filter_input(INPUT_GET, 'query', FILTER_SANITIZE_STRING);
+
+//Create a PHP array with our search criteria
+$findCriteria = [
+    '$text' => [ '$search' => $search_string ] 
+ ];
+
+//Find all of the customers that match  this criteria
+$cursor = $db->products->find($findCriteria);
+
+//Output the results
+echo "<h1>Results</h1>";
+foreach ($cursor as $product){
+   echo "<p>";
+   echo "<img src=" . $product['item_image']. ">";
+   echo $product['name'];
+   echo $product['price'];
+   echo "</p>";
+}
+
+//Close the connection
+$mongoClient->close();
