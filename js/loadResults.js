a83bcd50bb685df17eb9561963874ff8d7019541
Cristina Tincu <33434867+cristinatincu@users.noreply.github.com>
2019-02-22 11:16:31 +0000

    Add files via upload

--- js/loadResults.js
+++ js/loadResults.js
@@ -4,40 +4,26 @@ function loadItems() {
 
     //Create event handler that specifies what should happen when server responds
     request.onload = function(){
+
         //Check HTTP status code
         if(request.status === 200){
+
             //Get data from server
             var responseData = request.responseText;
 
             //Add data to page
-            displayProducts(responseData);
+            document.getElementById("shopItemsContainer").innerHTML = responseData;
         }
         else
             alert("Error communicating with server: " + request.status);
     };
 
     //Set up request with HTTP method and URL 
-    request.open("GET", "../functions/product_search.php");
+    request.open("POST", "../functions/product_search.php");
+
+    //Get query from the input
+    var query = document.getElementById("query").value;
 
     //Send request
-    request.send();
-}
-
-function displayProducts(jsonProducts){
-    //Convert JSON to array of product objects
-    var prodArray = JSON.parse(jsonProducts);
-    
-    //Create HTML table containing product data
-    var htmlStr = "";
-    for(var i=0; i<prodArray.length; ++i){
-        htmlStr += "<div class = 'shopItem'>";
-        htmlStr += "<p>" + prodArray[i].name + "</p>";
-        htmlStr += "<p><img src='" + prodArray[i].item_image + "'></p>";
-        htmlStr += "<p>£" + prodArray[i].description + "</p>";
-        htmlStr += "<p>£" + prodArray[i].price + "</p>";
-        htmlStr +="<button type = 'button' name = 'addItemButton' id = 'addItemButton1'>Add to basket</button>"
-        htmlStr += "</div>";
-    }
-    //Finish off table and add to document
-    document.getElementById("products").innerHTML = htmlStr;
+    request.send("query=" + query);
 }
